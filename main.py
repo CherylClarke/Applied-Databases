f5b376568a3b82edc1db8e024d349bc78ef33ab5
Cheryl Clarke <cherylclark90@live.co.uk>
2026-05-04 00:05:56 +0100

    neo4j log issue

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 # PYTHON APPLICATIONS
 
-## MySQL
+## MySQL= the data
 # copy and paste the code from the appdbproj.sql file into MYSQL in vm
 # check it worked by input : SELECT * FROM attendee;
 
-
+# Neo4j = connections/relationships
+## rename in neo4j admin to appdbproj
+## in cmd line run : C:Users\appDB\Documents\neo4j-community-5.26.19\bin>neo4j.bat console
+## localhost:7474\
+## ISSUE WITH LOGGG IN 
