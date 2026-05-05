25cbd3c62c4f2ef87c800cdb405fd309fb05dfae
Cheryl Clarke <cherylclark90@live.co.uk>
2026-05-05 21:47:44 +0100

    connection

--- main.py
+++ main.py
@@ -5,8 +5,12 @@
 # check it worked by input : SELECT * FROM attendee;
 
 # Neo4j = connections/relationships
-## rename in neo4j admin to appdbproj
+## rename in neo4j admin to appdbproj and un comment the code
+##  go to bin folder ,open cmd
 ## in cmd line run : C:Users\appDB\Documents\neo4j-community-5.26.19\bin>neo4j.bat console
-## localhost:7474\
+## go to = localhost:7474\
 ## select database appdbproj, paste code in from appdbproj.json file into the query box and run it
+
+# Conect to MySQL from python
+
 
